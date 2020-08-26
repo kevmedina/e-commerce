d7bd58e1c8030f545e874129d8c968c3d88ceafb
Kevin Medina <kmedinakm@hotmail.com>
2020-08-25 21:15:02 -0400

    deleted unnecessary files and code to start over fresh

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/Navbar/Navbar";
-import Landing from "./components/Landing/Landing";
-import Products from "./components/Products/Products";
-import ProductDetails from "./components/ProductDetails/ProductDetails";
-import Cart from "./components/Cart/Cart";
-import Default from "./components/Default";
 
 const App = () => {
   return (
-    <React.Fragment>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/products" component={Products} />
-        <Route exact path="/product-details" component={ProductDetails} />
-        <Route exact path="/cart" component={Cart} />
-        <Route component={Default} />
-      </Switch>
-    </React.Fragment>
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>Product List</main>
+      <footer>All right is reserved.</footer>
+    </div>
   );
 };
 
