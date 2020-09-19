92f040266a1faaa993be3114a7cbe89b77f4ede5
Kevin Medina <kmedinakm@hotmail.com>
2020-09-19 11:44:44 -0400

    add localStorage to the store to persist the data

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,4 +3,32 @@ import thunk from "redux-thunk";
 
 import { rootReducer } from "./reducers/rootReducer";
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+const saveToLocalStorage = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const loadFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
+};
+
+const persistedState = loadFromLocalStorage();
+
+export const store = createStore(
+  rootReducer,
+  persistedState,
+  applyMiddleware(thunk)
+);
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
